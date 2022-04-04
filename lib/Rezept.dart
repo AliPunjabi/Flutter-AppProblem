8b2d6118d592b7b8b04b9db9ac012d4bbe57b61a
Ali Punjabi <you@example.com>
2022-04-04 20:39:32 +0500

    check now

--- lib/Rezept.dart
+++ lib/Rezept.dart
@@ -2,34 +2,31 @@ import 'package:flutter/material.dart';
 import 'Sachen.dart';
 import 'dart:math';
 
-
-class Rezept extends StatelessWidget{
+class Rezept extends StatelessWidget {
   final String rezept;
-  final String name;//new changes
-  Rezept(this.rezept,this.name);
-
+  final String name; //new changes
+  Rezept(this.rezept, this.name);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF4caf50),
-        title: Text(name),//before changes it was Text(rezept)
+        title: Text(name), //before changes it was Text(rezept)
         foregroundColor: Colors.white,
         titleTextStyle: const TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 20,
         ),
       ),
-      body: RezeptPage(),
+      body: RezeptPage(rezept),
     );
   }
 }
 
-
-
 class RezeptPage extends StatefulWidget {
-  const RezeptPage ({Key? key}) : super(key: key);
+  String rezept;
+  RezeptPage(this.rezept, {Key? key}) : super(key: key);
 
   @override
   _RezeptPageState createState() => _RezeptPageState();
@@ -38,23 +35,34 @@ class RezeptPage extends StatefulWidget {
 class _RezeptPageState extends State<RezeptPage> {
   int PiNumber = 1;
 
-
   void changeDiceNumber() {
     setState(() {
-      PiNumber = Random().nextInt(4) +1;
+      PiNumber = Random().nextInt(4) + 1;
     });
   }
 
-
   final List<Sachen> infoBank = [
-    Sachen(title: "Schoko-Bowl", image: Image.asset("Images/frue1.png"), text: '-Mehl'),
-    Sachen(title: "Smoothie-Bowl", image: Image.asset("images/frue2.png"), text: '-Zucker'),
-    Sachen(title: "Porridge", image: Image.asset("images/frue3.png"), text: '-Milch'),
-    Sachen(title: "Porridge", image: Image.asset("images/frue4.png"), text: '-Eier'),
-    Sachen(title: "Schoko Mousse", image: Image.asset("images/frue5.png"),text: '-Butter'),
-
+    Sachen(
+        title: "Schoko-Bowl",
+        image: Image.asset("Images/frue1.png"),
+        text: '-Mehl'),
+    Sachen(
+        title: "Smoothie-Bowl",
+        image: Image.asset("images/frue2.png"),
+        text: '-Zucker'),
+    Sachen(
+        title: "Porridge",
+        image: Image.asset("images/frue3.png"),
+        text: '-Milch'),
+    Sachen(
+        title: "Porridge",
+        image: Image.asset("images/frue4.png"),
+        text: '-Eier'),
+    Sachen(
+        title: "Schoko Mousse",
+        image: Image.asset("images/frue5.png"),
+        text: '-Butter'),
   ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +75,8 @@ class _RezeptPageState extends State<RezeptPage> {
               elevation: 8,
               child: ListTile(
                 title: Center(
-                  child: Text(infoBank[PiNumber].title,
+                  child: Text(
+                    widget.rezept,
                   ),
                 ),
               ),
@@ -82,37 +91,44 @@ class _RezeptPageState extends State<RezeptPage> {
             margin: EdgeInsets.fromLTRB(10, 5, 10, 20),
             child: Padding(
               padding: EdgeInsets.fromLTRB(15, 15, 10, 0),
-              child: RichText(text: const TextSpan(
-                text: 'Zutaten:',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 22.0,
-                  decoration: TextDecoration.underline,
-                  decorationThickness: 2.0,
+              child: RichText(
+                text: const TextSpan(
+                  text: 'Zutaten:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 22.0,
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 2.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\n\n-Mehl'
+                          '\n-Zucker'
+                          '\n-Milch'
+                          '\n-Eier'
+                          '\n-Butter',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '\n\nZubereitung:',
+                    ),
+                    TextSpan(
+                      text: '\n\n-Zuerst vermischen wir alles'
+                          '\n-Anschließend wird es in der Pfanne angebraten'
+                          '\n-Zuletzt mit Schokolade bestreichen und genießen',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                  ],
                 ),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: '\n\n-Mehl' '\n-Zucker' '\n-Milch' '\n-Eier' '\n-Butter',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  TextSpan(
-                    text: '\n\nZubereitung:',
-                  ),
-                  TextSpan(
-                    text: '\n\n-Zuerst vermischen wir alles' '\n-Anschließend wird es in der Pfanne angebraten' '\n-Zuletzt mit Schokolade bestreichen und genießen',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal,
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                ],
-              ),
               ),
             ),
           ),
